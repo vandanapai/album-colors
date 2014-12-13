341adf2b8fa1b37abcac29a7a14e2843d8b6306a
Vandana Pai <vandana@vertigo.com>
2014-12-12 16:14:22 -0800

    added ajax call to get album tracks

--- js/album-colors.js
+++ js/album-colors.js
@@ -30,7 +30,6 @@ $( window ).load(function() {
 				currentAlbum.cover = data.albums.items[i].images[1].url;
 				currentAlbum.num = data.albums.items[i].id;
 
-
 				if (i > 0) {
 					if (previousAlbum == currentAlbum[i]) {
 					}
@@ -90,37 +89,32 @@ $( window ).load(function() {
 			var imgObject = data[k];
 
 			$('#mainContainer').append('<div id="colorContainer' + k + '"></div>'); // create a new square div for the color
-
 			$('#colorContainer' + k).addClass('img-color').append('<div id="albumContainer' + k + '"></div>');
-
 
 			var colorThief = new ColorThief();
 			var domColor = colorThief.getColor(imgObject);
 
 			$('#colorContainer' + k + '.img-color').css('background-color', 'rgb(' + domColor[0] + ',' + domColor[1] + ',' + domColor[2] + ')');  // display the dominant color
-
 			$('.songContainer').css('background-color', 'rgb(' + domColor[0] + ',' + domColor[1] + ',' + domColor[2] + ')'); // load dom color bg for modal
-
 			$('#albumContainer' + k).addClass('album-img-wrapper').append('<img src="' + imgObject.src + '" title="' + imgObject.num + '"></img>');
-			// $('.loaded-img').prepend(imgObject);
 
 		};
 
 		$('img').click(function() {
 			getSongs(this.title);
-			// $('.img-container').fadeIn(200);
 		});
-
-		// $('.close-button').click(function() {
-		// 	$('.img-container').fadeOut(200);
-		// });		
-
 
 	};
 
 	function getSongs(id) {
-
-
+		$.ajax ({
+			type: "GET",
+			url: "https://api.spotify.com/v1/albums/" + id + "/tracks",
+			dataType: "JSON",
+			success: function (songs) {
+				console.log(songs);
+			}
+		});
 	}
 
 });
